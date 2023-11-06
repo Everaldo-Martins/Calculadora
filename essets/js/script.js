4d3c3007d1fcda5fe0bbd7ccd638d56dbf70f30e
Everaldo-Martins <everaldoinfortecnico@gmail.com>
2023-11-06 10:53:27 -0300

    New Update

--- essets/js/script.js
+++ essets/js/script.js
@@ -1,40 +1,27 @@
 var calc = document.querySelector('.calc'),
-    result = document.querySelector('.result');
+    result = document.querySelector('.result')
+    num = ['1','2','3','4','5','6','7','8','9'];
 
     calc.value = '0';
     calc.focus();
 
-document.addEventListener('keypress', function(e){
-    let num = ['1','2','3','4','5','6','7','8','9'];
-    if(calc.value === '0' & e.key in num){
-        calc.value = ""
-    }
-    if(e.key === 'Enter'){
-        igual();
-    }
+document.addEventListener('keypress', function(e){    
+    if(calc.value === '0' & e.key in num) calc.value = ""
+    if(e.key === 'Enter') igual();
 }, false);
 
 function calcular(event){
-    let ch = ['1','2','3','4','5','6','7','8','9'];
     if(calc.value.length < 14){
-        if(calc.value === '0' & event in ch | event === '√'){
-            calc.value = event;
-        }
-        else{
-            calc.value += event;
-        }
+        if(calc.value === '0' & event in num | event === '√') calc.value = event;
+        else calc.value += event;
     }    
 }        
 
 function conhex(c){
     const l = {'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15};
     for(let i in l){
-        if(c === i){
-            return l[i];
-        }    
-        else if(c === l[i]){
-            return i;
-        }
+        if(c === i) return l[i];
+        else if(c === l[i]) return i;
     }          
     return c;
 }
@@ -49,6 +36,8 @@ function igual(){
 
     for(let i in numCalc){
         if(isNaN(numCalc[i]) & numCalc[i] != "." & numCalc[i] != ","){
+            if(numCalc[i] === "×" | numCalc[i] === "x") numCalc[i] = "*";
+            else if(numCalc[i] === "÷") numCalc[i] = "/";
             op = numCalc[i];
             count++;
         }
@@ -60,18 +49,19 @@ function igual(){
         }
         else if(count === 1){
             n1 += numCalc[i];
-        }               
-    }
+        } 
+        
+        n0 = parseFloat(n0);
+        n1 = parseFloat(n1);
 
-    n0 = parseFloat(n0);
-    n1 = parseFloat(n1);
-
-    if (op === "+") {r = n0 + n1;}
-    else if(op === "-"){r = n0 - n1;}
-    else if(op === "×" | op === "*"){r = n0 * n1;}
-    else if(op === "÷" | op === "/"){r = n0 / n1;}
-    else if(op === "√"){r = Math.sqrt(n1);}
-    else if(op === "%"){r = (n0 * n1) / 100;}
+        if(op === "*"){r = n0 * n1;}
+        else if(op === "/"){r = n0 / n1;}
+        else if (op === "+") {r = n0 + n1;}
+        else if(op === "-"){r = n0 - n1;}
+        
+        else if(op === "√"){r = Math.sqrt(n1);}
+        else if(op === "%"){r = (n0 * n1) / 100;}
+    }  
     
     result.value = calc.value;
     calc.value = r.toString().substring(0, 14);      
